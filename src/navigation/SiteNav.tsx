b233466871c4dc12d4c7abdabb446fce15267826
LukeSideris <LukeSideris@gmail.com>
2024-02-26 21:25:40 -0500

    Hooking up the site nav

--- src/navigation/SiteNav.tsx
+++ src/navigation/SiteNav.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Button, Menu, MenuItem, MenuTrigger, Popover } from 'react-aria-components';
+
+
+import classes from './SiteNav.module.css';
 
 function SiteNav() {
   return (
-    <nav>
-      <ul role="menu">
-        <li role="menuitem">
-          <NavLink to="/about/">About</NavLink>
+    <nav className={classes.siteNav}>
+      <ul>
+        <li>
+          <MenuTrigger>
+            <Button >
+              ☰ Menu
+            </Button>
+            <Popover>
+              <Menu>
+                <MenuItem className={classes.link} href="/">Home</MenuItem>
+                <MenuItem className={classes.link} href="/about/">About</MenuItem>
+              </Menu>
+            </Popover>
+          </MenuTrigger>
         </li>
       </ul>
     </nav>
