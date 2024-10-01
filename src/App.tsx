76e3b62c0e7378557c35190614229bdf624f0c7e
Luke Sideris <lukesideris@gmail.com>
2024-10-01 17:31:37 -0400

    About page and gitlab SPA redirect scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { RouterProvider } from 'react-aria-components';
 
 import SiteNav from './navigation/SiteNav';
@@ -8,9 +8,12 @@ import classes from './App.module.css';
 
 function App() {
   const navigate = useNavigate();
+  const { pathname = '/' } = useLocation();
+  const basePath = pathname.split('/')[1];
+
   return (
     <RouterProvider navigate={navigate}>
-      <div className={classes.app}>
+      <div className={`${classes.app} page-${basePath || 'home'}`}>
         <SiteNav />
 
         <main>
